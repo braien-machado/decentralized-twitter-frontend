365387dff11a578cd329986fd5f074f83255626c
Braien Machado de Paula <braienmp@outlook.com>
2025-01-15 14:35:28 -0300

    chore: accept default button props in custom button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,16 @@
+import type { ButtonHTMLAttributes } from 'react';
+
 type Props = {
   loading?: boolean;
   text: string;
-};
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ loading, text }: Props) => {
+export const Button = ({ loading, text, ...props }: Props) => {
   return (
     <button
+      {...props}
       type="button"
-      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-fit"
     >
       {loading ? 'Loading...' : text}
     </button>
