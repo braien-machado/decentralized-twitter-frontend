60eb8a941acf26d4776ae03ea194e9c2163e7de1
Braien Machado de Paula <braienmp@outlook.com>
2025-01-15 14:35:54 -0300

    fix: use english in post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,9 +7,9 @@ type Props = {
 export const Post = ({ address, message, timestamp }: Props) => {
   return (
     <div className="border border-gray-700 p-4 rounded mb-2">
-      <div className="truncate">Carteira: {address}</div>
-      <div>Data: {timestamp}</div>
-      <div>Mensagem: {message}</div>
+      <div className="truncate">Wallet: {address}</div>
+      <div>Date: {timestamp}</div>
+      <div>Message: {message}</div>
     </div>
   );
 };
